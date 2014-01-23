547468e20e5c006b91c26df1d0d1162dfc5ed891
nigulasikk <qiankaijie@126.com>
2014-01-23 11:05:13 +0800

    init
    first time

---
+++ js/index.js
@@ -0,0 +1,23 @@
+//$(".login").click(function(){
+//    $( "body" ).prepend( '<div class="quanping" style="z-index:2"></div>' );
+//    $(".quanping").after("<div id='edit-div'></div>");
+//    window.location.reload();
+////    $("#edit").load("play.html",null,function(response, status, xml){
+////        if ( status === "success") {
+////
+////        }   else{
+////            alert("加载页面失败！");
+////            $("#edit-div, .quanping").remove();
+////        }
+////    });
+//});
+$(document).ready(function(){
+    jQuery(".carousel").carousel({
+        interval: 5000
+    });
+
+
+
+
+
+});
